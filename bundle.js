8bf279bb8497486781e2fdd5e5f8bac55499e7c8
danielmoi <daniel@ampersandmoi.com>
2016-06-05 14:54:36 +1000

    Bundle using Webpack

--- bundle.js
+++ bundle.js
@@ -1,106 +1,169 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-/**
- * The base implementation of `_.sum` and `_.sumBy` without support for
- * iteratee shorthands.
- *
- * @private
- * @param {Array} array The array to iterate over.
- * @param {Function} iteratee The function invoked per iteration.
- * @returns {number} Returns the sum.
- */
-function baseSum(array, iteratee) {
-  var result,
-      index = -1,
-      length = array.length;
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
 
-  while (++index < length) {
-    var current = iteratee(array[index]);
-    if (current !== undefined) {
-      result = result === undefined ? current : (result + current);
-    }
-  }
-  return result;
-}
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
 
-module.exports = baseSum;
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
 
-},{}],2:[function(require,module,exports){
-/**
- * This method returns the first argument given to it.
- *
- * @static
- * @since 0.1.0
- * @memberOf _
- * @category Util
- * @param {*} value Any value.
- * @returns {*} Returns `value`.
- * @example
- *
- * var object = { 'user': 'fred' };
- *
- * console.log(_.identity(object) === object);
- * // => true
- */
-function identity(value) {
-  return value;
-}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
 
-module.exports = identity;
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
 
-},{}],3:[function(require,module,exports){
-var baseSum = require('./_baseSum'),
-    identity = require('./identity');
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
 
-/**
- * Computes the sum of the values in `array`.
- *
- * @static
- * @memberOf _
- * @since 3.4.0
- * @category Math
- * @param {Array} array The array to iterate over.
- * @returns {number} Returns the sum.
- * @example
- *
- * _.sum([4, 2, 8, 6]);
- * // => 20
- */
-function sum(array) {
-  return (array && array.length)
-    ? baseSum(array, identity)
-    : 0;
-}
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
 
-module.exports = sum;
 
-},{"./_baseSum":1,"./identity":2}],4:[function(require,module,exports){
-// require our module
-var lib = require('./lib.js');
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
 
-// use the double function from our module
-console.log(lib.double(2));
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
 
-// use the addFive function from our module
-console.log(lib.addFive(2));
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
 
-},{"./lib.js":5}],5:[function(require,module,exports){
-// require sum from lodash
-var sum = require('lodash/sum');
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
 
-// our first module
-var double = function(num) {
-  return num * 2;
-}
+	// require our module (IMPORT)
+	var lib = __webpack_require__(1);
 
-// use sum from lodash
-var addFive = function(num) {
-  return sum([num, 5]);
-}
+	// use the double function from our module
+	console.log(lib.double(2));
 
-// Exports
-module.exports = {
-  double: double,
-  addFive: addFive
-}
+	// use the addFive function from our module
+	console.log(lib.addFive(2));
 
-},{"lodash/sum":3}]},{},[4]);
+
+/***/ },
+/* 1 */
+/***/ function(module, exports, __webpack_require__) {
+
+	// require sum from lodash (IMPORT)
+	var sum = __webpack_require__(2);
+
+	// our first module
+	var double = function(num) {
+	  return num * 2;
+	}
+
+	// use sum from lodash
+	var addFive = function(num) {
+	  return sum([num, 5]);
+	}
+
+	// our exports (EXPORT)
+	module.exports = {
+	  double: double,
+	  addFive: addFive
+	}
+
+
+/***/ },
+/* 2 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var baseSum = __webpack_require__(3),
+	    identity = __webpack_require__(4);
+
+	/**
+	 * Computes the sum of the values in `array`.
+	 *
+	 * @static
+	 * @memberOf _
+	 * @since 3.4.0
+	 * @category Math
+	 * @param {Array} array The array to iterate over.
+	 * @returns {number} Returns the sum.
+	 * @example
+	 *
+	 * _.sum([4, 2, 8, 6]);
+	 * // => 20
+	 */
+	function sum(array) {
+	  return (array && array.length)
+	    ? baseSum(array, identity)
+	    : 0;
+	}
+
+	module.exports = sum;
+
+
+/***/ },
+/* 3 */
+/***/ function(module, exports) {
+
+	/**
+	 * The base implementation of `_.sum` and `_.sumBy` without support for
+	 * iteratee shorthands.
+	 *
+	 * @private
+	 * @param {Array} array The array to iterate over.
+	 * @param {Function} iteratee The function invoked per iteration.
+	 * @returns {number} Returns the sum.
+	 */
+	function baseSum(array, iteratee) {
+	  var result,
+	      index = -1,
+	      length = array.length;
+
+	  while (++index < length) {
+	    var current = iteratee(array[index]);
+	    if (current !== undefined) {
+	      result = result === undefined ? current : (result + current);
+	    }
+	  }
+	  return result;
+	}
+
+	module.exports = baseSum;
+
+
+/***/ },
+/* 4 */
+/***/ function(module, exports) {
+
+	/**
+	 * This method returns the first argument given to it.
+	 *
+	 * @static
+	 * @since 0.1.0
+	 * @memberOf _
+	 * @category Util
+	 * @param {*} value Any value.
+	 * @returns {*} Returns `value`.
+	 * @example
+	 *
+	 * var object = { 'user': 'fred' };
+	 *
+	 * console.log(_.identity(object) === object);
+	 * // => true
+	 */
+	function identity(value) {
+	  return value;
+	}
+
+	module.exports = identity;
+
+
+/***/ }
+/******/ ]);
