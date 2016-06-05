f8fb9c97b15f7e05cafc20e6e72ba23969634562
danielmoi <daniel@ampersandmoi.com>
2016-06-05 13:17:31 +1000

    First module using browserify

---
+++ src/entry.js
@@ -0,0 +1,5 @@
+// require our module
+var lib = require('./lib.js');
+
+// use our module
+console.log(lib.double(2));
---
+++ src/lib.js
@@ -0,0 +1,8 @@
+// our first module
+var double = function(num) {
+  return num * 2;
+}
+
+module.exports = {
+  double: double
+}
